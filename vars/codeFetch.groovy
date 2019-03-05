8c62848f46c0eab403c23a3f4a598b24d9c7df05
checkacer <44667785+checkacer@users.noreply.github.com>
2019-03-05 18:42:01 +0800

    Update codeFetch.groovy

--- vars/codeFetch.groovy
+++ vars/codeFetch.groovy
@@ -5,20 +5,32 @@ def call(String a){
    }else{
      echo "-----------------bbb"
    }
-   pipeline {
-     agent any
-     environment {
-       abc = "abc"
+   node {
+     def mvnHome
+     def jdkHome
+     stage('Preparation') { 
+        git credentialsId: '13735461-01df-48bf-85cc-373338e73227', url: 'https://github.com/checkacer/runindockerdemo'      
+        mvnHome = tool 'maven'
+        jdkHome = tool 'jdk'
      }
-     tools {
-       jdk 'jdk'
+     stage('Package') {
+        sh "'${mvnHome}/bin/mvn' package"
      }
-     stages {
-       stage('in library') {
-         steps {
-	   echo "++++++++ in ++++++"
-	 }       
-       }
+     def imagesName = 'registry.cn-hangzhou.aliyuncs.com/dcits/runindocker:v0.1'  
+     stage('Push') {
+         docker.build("registry.cn-hangzhou.aliyuncs.com/dcits/runindocker:v0.1",'.')
      }
-   }
+     stage('Deploy') {
+        try{
+          sh 'docker rm -f runindocker'
+        }catch(e){
+          // err message
+        }
+        docker.image(imagesName).run('-d --restart=unless-stopped -p 9091:80 --name runindocker') 
+     }
+     stage('Results') {
+        archive 'target/*.jar'
+	junit 'target/surefire-reports/TEST-*.xml'
+     }
+  }
 }
