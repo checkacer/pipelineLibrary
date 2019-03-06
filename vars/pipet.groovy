11a1b1aec17e2dc4469dedf696f04180a135b249
checkacer <44667785+checkacer@users.noreply.github.com>
2019-03-06 17:55:16 +0800

    Update pipet.groovy

--- vars/pipet.groovy
+++ vars/pipet.groovy
@@ -2,9 +2,7 @@ def call(){
     pipeline {
         agent any
         environment {
-            tag = "v0.3.${BUILD_NUMBER}"
             registry = "registry.cn-hangzhou.aliyuncs.com"
-            imageName = "dcits/i-test:${tag}"
             gitCredentialsId = "a6600fad-d566-4408-b024-2d5e8ea29311"
             dockerCredential = "aliyun"
             branch = "master"
@@ -14,9 +12,12 @@ def call(){
             maven 'maven'
             jdk 'jdk'
         }
-        def utils = new org.foo.Utils(steps)
-        stage('build') {
-            utils.checkOutFromGit("${branch}","${gitCredentialsId}","${repoUrl}")
+        stages {
+            stage('test') {
+                steps {
+                    echo "Hello World"
+                }
+            }
         }
 
     }
