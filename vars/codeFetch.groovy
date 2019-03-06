19cd95cee18fbd3742ef5eeeef51e853b89be43e
checkacer <44667785+checkacer@users.noreply.github.com>
2019-03-06 09:25:26 +0800

    Update codeFetch.groovy

--- vars/codeFetch.groovy
+++ vars/codeFetch.groovy
@@ -14,7 +14,7 @@ def call(String giturl){
         jdkHome = tool 'jdk'
      }
      stage('Package') {
-        sh "'${mvnHome}/bin/mvn' package"
+        sh "'${mvnHome}/bin/mvn' package -X "
      }
       
      stage('Results') {
