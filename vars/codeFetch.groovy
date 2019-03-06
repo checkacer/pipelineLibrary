04c9d5e4cba5b5cbf9374a8a05fbb47c2bf19ddd
checkacer <44667785+checkacer@users.noreply.github.com>
2019-03-06 14:25:01 +0800

    Update codeFetch.groovy

--- vars/codeFetch.groovy
+++ vars/codeFetch.groovy
@@ -1,23 +1,23 @@
 #!groovy 
 def call(String giturl){
-   if(giturl == "a"){
-     echo "starting fetch code......"
-   }else{
-     echo "-----------------bbb"
-   }
-     def mvnHome
-     def jdkHome
-     stage('Preparation') { 
-        git credentialsId: '13735461-01df-48bf-85cc-373338e73227', url: giturl      
+    if(giturl == "a"){
+        echo "starting fetch code......"
+    }else{
+        echo "-----------------bbb"
+    }
+    def mvnHome
+    def jdkHome
+    stage('Preparation') {
+        git credentialsId: '13735461-01df-48bf-85cc-373338e73227', url: giturl
         mvnHome = tool 'maven'
         jdkHome = tool 'jdk'
-     }
-     stage('Package') {
+    }
+    stage('Package') {
         sh "'${mvnHome}/bin/mvn' package"
-     }
-      
-     stage('Results') {
+    }
+
+    stage('Results') {
         archive 'target/*.jar'
-	junit 'target/surefire-reports/*.xml'
-     }
+        junit 'target/surefire-reports/*.xml'
+    }
 }
